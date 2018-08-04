ab12b7a53a46c76432eabe7d349b3bd075d592c0
Krishna <kpmp091@gmail.com>
2018-08-04 16:38:24 -0700

    intial setup files

---
+++ src/main/java/com/krishna/spring/springgitdemo/SpringgitdemoApplication.java
@@ -0,0 +1,12 @@
+package com.krishna.spring.springgitdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringgitdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringgitdemoApplication.class, args);
+    }
+}
